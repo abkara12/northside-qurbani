4978611c59d20f70c57b608360b28e3f1a41c503
abkara12 <abdurrahmaanmahomed6@gmail.com>
2026-05-03 12:35:57 -0700

    save

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
+  clientsClaim: true,
+  cleanupOutdatedCaches: true,
   disable: process.env.NODE_ENV === "development",
 });
 
